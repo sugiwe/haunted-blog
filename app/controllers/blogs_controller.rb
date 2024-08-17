e8e2642fc92dd9acbd467c57fc818f5a1970d4e4
hiromisugie <lozz.gggg@gmail.com>
2024-08-18 05:00:29 +0900

    set_blogメソッドの修正が間違っていたので再修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -45,7 +45,7 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.where(id: params[:id]).where('user_id = ? OR secret = ?', current_user.id, false).first!
+    @blog = Blog.find_by!('id = ? AND (user_id = ? OR secret = ?)', params[:id], current_user&.id, false)
   end
 
   def authorize_blog
